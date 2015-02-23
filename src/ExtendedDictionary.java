8155207beb195b8ce60fb05e6adea9f53abdfc54
1309013 <1309013@RG41107.rgu.ac.uk>
2015-02-23 10:55:16 +0000

    Setup Extended Dictionary

---
+++ src/ExtendedDictionary.java
@@ -0,0 +1,18 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Lisa, Chris
+ * @version 1.0
+ * @date 23/2/15
+ */
+public class ExtendedDictionary {
+    map.put("Journal","a set of articles, usually bound with a soft cover");
+    map.put("Author","a person who writes");
+    
+}
