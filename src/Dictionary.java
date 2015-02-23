98c5a847dc54bc656c5a84d89e78571557a32b4e
1309013 <1309013@RG41107.rgu.ac.uk>
2015-02-23 10:21:18 +0000

    Setup Dictionary

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -3,21 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication14;
-
 /**
  *
  * @author Lisa, Chris
  * @version 1.0
  * @date 23/2/15
  */
-public class JavaApplication14 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
+public interface Dictionary {
     
 }
