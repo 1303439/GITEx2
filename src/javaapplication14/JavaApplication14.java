6b8df42c340b419c13537c32efd31cd2d4495c09
1303439 <1303439@RG41121.rgu.ac.uk>
2015-02-23 10:25:21 +0000

    Hmm

--- src/javaapplication14/JavaApplication14.java
+++ src/javaapplication14/JavaApplication14.java
@@ -17,7 +17,7 @@ public class JavaApplication14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("");
     }
     
 }
